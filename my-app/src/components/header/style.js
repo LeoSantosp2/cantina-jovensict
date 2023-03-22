e2f9f5af397132e15d8cc9174a8d012d5fdca143
Leonardo Santos <leogilberto2010@hotmail.com>
2023-03-22 10:57:09 -0300

    Estilizando o componente footer

---
+++ my-app/src/components/header/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        paddingTop: 20,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        zIndex: 2,
+        position: 'absolute',
+        elevation: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#f2f2f2',
+    },
+
+    img: {
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10,
+    },
+});
