b975a28f470113b4a77a911af9c4d5dc90f657dd
Leonardo Santos <leogilberto2010@hotmail.com>
2023-03-22 11:00:41 -0300

    Adicionando o componente torta

---
+++ my-app/src/components/torta/index.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { styles } from '../../styles/style';
+
+export default function Torta({ valuePrice }) {
+    const [count, setCount] = useState(0);
+    const [price, setPrice] = useState(0);
+
+    const handleClickUp = () => {
+        setCount(count + 1);
+
+        if (price === 0) {
+            setPrice(6);
+        } else {
+            setPrice(price * 2);
+        }
+    };
+
+    const handleClickDown = () => {
+        if (count === 0) return;
+
+        setCount(count - 1);
+
+        setPrice(price / 2);
+
+        if (count === 1) {
+            setPrice(price - price);
+        }
+    };
+
+    useEffect(() => {
+        valuePrice(price);
+    }, [price]);
+
+    return (
+        <View style={styles.foodOption}>
+            <Text style={styles.title}>Torta</Text>
+
+            <View style={styles.containerCount}>
+                <TouchableOpacity style={styles.button} onPress={() => handleClickUp()}>
+                    <Text>+</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.count}>{count}</Text>
+
+                <TouchableOpacity style={styles.button} onPress={() => handleClickDown()}>
+                    <Text>-</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.value}>R${price.toFixed(2).replace('.', ',')}</Text>
+            </View>
+        </View>
+    );
+}
